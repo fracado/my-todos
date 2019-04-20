8c70f1b7c370c375615ccfb438512ba5b4703bd9
fbath <phoenixa@gmx.de>
2019-04-20 20:27:44 +0200

    added check and remove functions to task list

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,6 +2,7 @@ import { Template } from 'meteor/templating';
 
 import { Tasks } from '../api/tasks.js';
 
+import './task.js';
 import './body.html';
 
 // defined 'tasks' helper to return tasks from collection
@@ -9,28 +10,28 @@ Template.body.helpers({
   tasks() {
     // Show newest tasks at the top
     return Tasks.find({}, { sort: { createdAt: -1 } });
-  },
+  }
 });
 
 // add event listeners
 Template.body.events({
-  // event handler for ADD TASK form 
+  // event handler for ADD TASK form
   // (listening to 'submit' of new-task input-field)
   'submit .new-task'(event) {
     // Prevent default browser form submit
     event.preventDefault();
- 
+
     // Get value from form element
     const target = event.target;
     const text = target.text.value;
- 
+
     // Insert a task into the collection
     Tasks.insert({
       text,
-      createdAt: new Date(), // current time
+      createdAt: new Date() // current time
     });
- 
+
     // Clear form
     target.text.value = '';
-  },
+  }
 });
