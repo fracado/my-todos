14341883c6464339fc8e0d8cd77d2309f07f232c
fbath <phoenixa@gmx.de>
2019-04-20 20:11:14 +0200

    added form to add new tasks

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -7,6 +7,30 @@ import './body.html';
 // defined 'tasks' helper to return tasks from collection
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    // Show newest tasks at the top
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+// add event listeners
+Template.body.events({
+  // event handler for ADD TASK form 
+  // (listening to 'submit' of new-task input-field)
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+ 
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+ 
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+ 
+    // Clear form
+    target.text.value = '';
   },
 });
